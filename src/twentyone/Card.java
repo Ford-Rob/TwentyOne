0a9ae63e05372389a64a12bdf47d5fb0deb25a05
RFord <RFord@FORD-W520.Turbomeca-USA.com>
2013-10-10 20:53:13 -0500

    Added Card Class

--- src/twentyone/Card.java
+++ src/twentyone/Card.java
@@ -8,8 +8,10 @@ package twentyone;
  * @author RFord
  */
 public class Card {
-    String Suit = "Clubs";
+    
     String Rank = "10";
+    char Suit = 9827; // 9827 Club, 9824 Spade, 9829 Heart, 9830 Diamond
+    
     
     public void displayCard() {
         System.out.println("\n" + this.Rank + " of " + this.Suit + "\n");
--- src/twentyone/GameMenu.java
+++ src/twentyone/GameMenu.java
@@ -9,13 +9,25 @@ package twentyone;
  * @author RFord
  */
 public class GameMenu {
+    short SuitNumber = 9824;
     String gameMenuItem1 = "\nH-Hit";
     String gameMenuItem2 = "\nS-Stand";
     String gameMenuItem3 = "\nQ-Quit";
+    byte counter = 0;
+    char border;
+    
         public void displayGameMenu(){
-        System.out.println("\nGame Menu\n\n ");
-        System.out.println(this.gameMenuItem1);
-        System.out.println(this.gameMenuItem2);
-        System.out.println(this.gameMenuItem3);
+            while (++counter < 25){
+                    border=(char)SuitNumber;
+                    System.out.print(border);
+                    if (SuitNumber > 9830) {    
+                        SuitNumber = 9824;
+                    } else {++SuitNumber;
+                    }      
+            }                           
+            System.out.println("\n\tGame Menu ");
+            System.out.println(this.gameMenuItem1);
+            System.out.println(this.gameMenuItem2);
+            System.out.println(this.gameMenuItem3);
     }
 }
