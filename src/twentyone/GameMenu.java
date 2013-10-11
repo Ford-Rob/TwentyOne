78fb524f8592574b253bfd5644b1071192696f4b
RFord <RFord@FORD-W520.Turbomeca-USA.com>
2013-10-10 21:20:32 -0500

    Updated GameMenu with border

--- src/twentyone/GameMenu.java
+++ src/twentyone/GameMenu.java
@@ -10,9 +10,9 @@ package twentyone;
  */
 public class GameMenu {
     short SuitNumber = 9824;
-    String gameMenuItem1 = "\nH-Hit";
-    String gameMenuItem2 = "\nS-Stand";
-    String gameMenuItem3 = "\nQ-Quit";
+    String gameMenuItem1 = "\n\tH-Hit";
+    String gameMenuItem2 = "\tS-Stand";
+    String gameMenuItem3 = "\tQ-Quit";
     byte counter = 0;
     char border;
     
@@ -26,8 +26,23 @@ public class GameMenu {
                     }      
             }                           
             System.out.println("\n\tGame Menu ");
+            counter = 0;
+            while (++counter < 26){
+                    border=(char)SuitNumber;
+                    System.out.print("-");
+ 
+            }  
             System.out.println(this.gameMenuItem1);
             System.out.println(this.gameMenuItem2);
             System.out.println(this.gameMenuItem3);
+            counter = 0;
+            while (++counter < 25){
+                    border=(char)SuitNumber;
+                    System.out.print(border);
+                    if (SuitNumber > 9830) {    
+                        SuitNumber = 9824;
+                    } else {++SuitNumber;
+                    }      
+            }  
     }
 }
