9f48221ec8a0732a2fe80dd7f82c468fe1276d85
RFord <RFord@FORD-W520.Turbomeca-USA.com>
2013-10-31 09:43:06 -0500

    Updated GameMenu, Hand, Card

--- src/twentyone/TwentyOne.java
+++ src/twentyone/TwentyOne.java
@@ -7,12 +7,17 @@ package twentyone;
 public class TwentyOne {
     public static Hand myHand = new Hand(); 
     public static Player myPlayer = new Player();
+    public static Deck myDeck = new Deck();
     
     public static void main(String[] args) {
         TwentyOne myGame = new TwentyOne();
         System.out.println("\nWelcome to 21.");
         myPlayer.multiPlayer();
         myPlayer.getName();
+        myDeck.initDeck();
+        myDeck.showDeck();
+        myDeck.shuffleDeck();
+        myDeck.showDeck();
         HelpMenuView myHelpMenuView = new HelpMenuView();
         myHelpMenuView.getInput();
         //Rob, Moved your code here to MainMenuView to diplay before GameMenu. The game is taking shape! I keep going bust. haha
