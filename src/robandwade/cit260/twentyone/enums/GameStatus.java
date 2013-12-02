fcc98633f1f12051a39a113e5e779ee01908940f
RFord <RFord@FORD-W520.Turbomeca-USA.com>
2013-12-02 16:58:43 -0600

    Updated GameMenu, Hand, Card

---
+++ src/robandwade/cit260/twentyone/enums/GameStatus.java
@@ -0,0 +1,16 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package robandwade.cit260.twentyone.enums;
+
+/**
+ *
+ * @author RFord
+ */
+public enum GameStatus {
+    Win,
+    Lose,
+    Bust,
+    Push,
+}
